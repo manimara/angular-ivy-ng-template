430c771cd742f01f988143ec1baaeb79a6af570a
manimara <manimaracbe@gmail.com>
2021-05-31 22:10:38 +0530

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit, VERSION } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  implements OnInit{
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
-  
-ngOnInit(){
-  
-}
+  formGroup: FormGroup;
+  controls = [{ name: 'fname' }];
+  val = '';
+  array = {
+    name: 'subjects',
+    controls: [
+      {
+        name: 'subject'
+      },
+      {
+        name: 'mark'
+      }
+    ]
+  };
+  subjects = [];
+  constructor() {}
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      fname: new FormControl(),
+      subjects: new FormArray([])
+    });
+  }
 
+  addSubjects() {
+    this.subjects.push(
+      new FormGroup({
+        subject: new FormControl(''),
+        mark: new FormControl('')
+      })
+    );
+  }
+
+  getFormValues() {
+    this.val = this.formGroup.getRawValue();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, 
+  FormsModule, CommonModule, ReactiveFormsModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
